9e333d23daa7d65ed7055ab827fe5cf798266d8c
Jessica <jessica.stpierre@hotmail.com>
2021-03-14 11:55:35 -0400

    - New input system player movement done

---
+++ Assets/Inputs/Inputs.cs
@@ -0,0 +1,263 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Inputs.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Inputs : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Inputs()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Inputs"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""bdc27aba-fc97-41f8-9fa4-60cd7ce39421"",
+            ""actions"": [
+                {
+                    ""name"": ""Forwards"",
+                    ""type"": ""Button"",
+                    ""id"": ""759bffef-c942-4842-9f20-44bd51123c59"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Backwards"",
+                    ""type"": ""Button"",
+                    ""id"": ""1bbebd86-4c69-4917-b687-6d1006434ab6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Left"",
+                    ""type"": ""Button"",
+                    ""id"": ""6d869b9e-a356-477a-b0ea-fcf41f0dddc8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Right"",
+                    ""type"": ""Button"",
+                    ""id"": ""65d86bcb-7955-4b62-8a47-abe7fdc3bb76"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""0621db94-323a-45b4-9a34-13ccb023183e"",
+                    ""path"": ""<Keyboard>/#(W)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Forwards"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""291eda68-c9ed-4c66-9af7-a6dca5149929"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Forwards"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6b11e2b0-e39d-4891-bac0-1a75c6218ddb"",
+                    ""path"": ""<Keyboard>/#(S)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Backwards"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""78a2e877-1304-4fb3-885d-df614f038764"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Backwards"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""60c130e7-7eed-41a8-baf9-debcb19798f3"",
+                    ""path"": ""<Keyboard>/#(A)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Left"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""205cbece-fe8f-44ca-8170-0d36075936fb"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Left"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""13576328-afb0-4b39-9e9f-b783d6ab8f2e"",
+                    ""path"": ""<Keyboard>/#(D)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Right"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b8e533f3-90b5-4ce6-9f88-e7df2032843a"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Right"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Forwards = m_Player.FindAction("Forwards", throwIfNotFound: true);
+        m_Player_Backwards = m_Player.FindAction("Backwards", throwIfNotFound: true);
+        m_Player_Left = m_Player.FindAction("Left", throwIfNotFound: true);
+        m_Player_Right = m_Player.FindAction("Right", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Forwards;
+    private readonly InputAction m_Player_Backwards;
+    private readonly InputAction m_Player_Left;
+    private readonly InputAction m_Player_Right;
+    public struct PlayerActions
+    {
+        private @Inputs m_Wrapper;
+        public PlayerActions(@Inputs wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Forwards => m_Wrapper.m_Player_Forwards;
+        public InputAction @Backwards => m_Wrapper.m_Player_Backwards;
+        public InputAction @Left => m_Wrapper.m_Player_Left;
+        public InputAction @Right => m_Wrapper.m_Player_Right;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Forwards.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnForwards;
+                @Forwards.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnForwards;
+                @Forwards.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnForwards;
+                @Backwards.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnBackwards;
+                @Backwards.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnBackwards;
+                @Backwards.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnBackwards;
+                @Left.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLeft;
+                @Left.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLeft;
+                @Left.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLeft;
+                @Right.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRight;
+                @Right.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRight;
+                @Right.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRight;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Forwards.started += instance.OnForwards;
+                @Forwards.performed += instance.OnForwards;
+                @Forwards.canceled += instance.OnForwards;
+                @Backwards.started += instance.OnBackwards;
+                @Backwards.performed += instance.OnBackwards;
+                @Backwards.canceled += instance.OnBackwards;
+                @Left.started += instance.OnLeft;
+                @Left.performed += instance.OnLeft;
+                @Left.canceled += instance.OnLeft;
+                @Right.started += instance.OnRight;
+                @Right.performed += instance.OnRight;
+                @Right.canceled += instance.OnRight;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnForwards(InputAction.CallbackContext context);
+        void OnBackwards(InputAction.CallbackContext context);
+        void OnLeft(InputAction.CallbackContext context);
+        void OnRight(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Inputs inputs;
+    public Inputs.PlayerActions playerActions;
+    [Tooltip("Speed of movement for all axis")]
+    [SerializeField] private float speed;
+    [Tooltip("Drag in the rigidbody attached to this object")]
+    [SerializeField] private Rigidbody rb;
+    private Vector3 newDir;
+    private float zDirection;
+    private float xDirection;
+
+    private void Awake() 
+    {
+        inputs = new Inputs();
+        playerActions = inputs.Player;
+        playerActions.Enable();
+    }
+
+    private void FixedUpdate() 
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        zDirection = DirectionOutput(playerActions.Forwards, playerActions.Backwards);
+        xDirection = DirectionOutput(playerActions.Right, playerActions.Left);
+        newDir = new Vector3(xDirection, 0f, zDirection);
+        Move(newDir.normalized);
+    }
+
+    private float DirectionOutput(InputAction posButton, InputAction negButton)
+    {
+        float DirOutput = 0f;
+
+        if(InputDetection(posButton) > 0 && InputDetection(negButton) > 0) DirOutput = 0f;
+        else if(InputDetection(posButton) > 0 || InputDetection(negButton) > 0)
+        {
+            if(InputDetection(posButton) > 0) DirOutput = InputDetection(posButton);
+            if(InputDetection(negButton) > 0) DirOutput = -InputDetection(negButton);
+        }
+        else DirOutput = 0f;
+
+        return DirOutput;
+    }
+
+    private float InputDetection(InputAction inputButton)
+    {
+        float dir = 0;
+        if(inputButton.ReadValue<float>() > 0f)
+        {
+            dir = inputButton.ReadValue<float>();
+        }
+        return dir;
+    }
+
+    private void Move(Vector3 direction)
+    {
+        Vector3 move = direction * speed * Time.deltaTime;
+        rb.MovePosition(this.transform.position + move);
+    }
+}
