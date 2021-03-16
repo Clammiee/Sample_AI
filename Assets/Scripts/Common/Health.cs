2f2da96900d6a9e863022482c61e877a01ced648
Jessica <jessica.stpierre@hotmail.com>
2021-03-16 15:50:35 -0400

    - Enemy AI now animates when hurt and dead

--- Assets/Scripts/Common/Health.cs
+++ Assets/Scripts/Common/Health.cs
@@ -11,8 +11,12 @@ public class Health : MonoBehaviour, IDamagable
     [SerializeField] private float health = 100f;
     [Tooltip("Drag in health bar as child of canvas")]
     [SerializeField] private Image healthBar;
+    [SerializeField] private float stopAnimTimer;
+    [Tooltip("Needs to be higher than Stop Anim Timer")]
+    [SerializeField] private float stayDeadTimer;
 
     private float healthDecimal;
+    private int dieOnce = 0;
 
     public void Damage(float amount)
     {
@@ -21,9 +25,34 @@ public class Health : MonoBehaviour, IDamagable
 
         if(health <= 0)
         {
-            this.gameObject.SetActive(false);
-            healthDecimal = 0f;
+            if(dieOnce == 0)
+            {
+                healthDecimal = 0f;
+                AnimationEvents.TriggerOnPlayAnimation(this.gameObject, "Dead", true);
+                //Add Interface that disables all script functionalities so that the AI or player stays dead
+                dieOnce++;
+            }
+            StartCoroutine(StopAnim(stopAnimTimer));
+            StartCoroutine(StayDead(stayDeadTimer));
+        }
+        else 
+        {
+            AnimationEvents.TriggerOnPlayAnimation(this.gameObject, "Hurt", true);
+            StartCoroutine(StopAnim(stopAnimTimer));
         }
         healthBar.fillAmount = healthDecimal;
+    }
+
+    private IEnumerator StopAnim(float time)
+    {
+        yield return new WaitForSeconds(time);
+        AnimationEvents.TriggerOnPlayAnimation(this.gameObject, "Hurt", false);
+        AnimationEvents.TriggerOnPlayAnimation(this.gameObject, "Dead", false);
+    }
+
+    private IEnumerator StayDead(float time)
+    {
+        yield return new WaitForSeconds(time);
+        AnimationEvents.TriggerOnPlayAnimation(this.gameObject, "Idle", false);
     }
 }
