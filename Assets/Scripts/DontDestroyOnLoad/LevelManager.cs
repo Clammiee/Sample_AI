3a1f2ef2da293a1e6260054e1a3a0de1d1733127
Jessica St-Pierre <jessica.stpierre@hotmail.com>
2021-03-20 10:38:42 -0400

    - Added toolbox singleton and levelManager

--- Assets/Scripts/DontDestroyOnLoad/LevelManager.cs
+++ Assets/Scripts/DontDestroyOnLoad/LevelManager.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class LevelManager : MonoBehaviour
 {
-    
+    public void ChangeLevel(int chosenLevel)
+    {
+        switch(chosenLevel)
+        {
+            case 1:
+                SceneManager.LoadScene("Level1");
+                break;
+            case 2:
+                SceneManager.LoadScene("Level2");
+                break;
+        }
+    }
 }
--- Assets/Scripts/DontDestroyOnLoad/Toolbox.cs
+++ Assets/Scripts/DontDestroyOnLoad/Toolbox.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Toolbox : MonoBehaviour
 {
-    
+    public static Toolbox Instance { get; private set; }
+    private LevelManager levelManager;
+
+    void Awake()
+    {
+        if (Instance == null) 
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else Destroy(gameObject);
+
+        var go1 = new GameObject("LevelManager");
+        go1.transform.parent = this.gameObject.transform;
+        this.levelManager = go1.AddComponent<LevelManager>();
+    }
+
+    public LevelManager GetLevelManager()//Toolbox.Instance.GetLevelManager()
+    {
+        return this.levelManager;
+    }
 }
